d9740bcf97bc45cc3217c56ef69d822598558494
jarg <josegoncalves@esjaloures.org>
2026-03-23 11:46:34 +0000

    Pré-condições.

--- 00_Intro/Conta.java
+++ 00_Intro/Conta.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 /**
- * Escreva uma descrição da classe Conta aqui.
- * 
- * @author (seu nome) 
- * @version (um número da versão ou uma data)
+ * Versão 2 da conta
+ * Introduzir pré‑condições (design by contract)
+ * Antes de executar um método, 
+ * deve‑se garantir o estado mínimo aceitável
  */
 public class Conta
 {
@@ -12,11 +12,24 @@ public class Conta
     public ArrayList<Movimento> movimentos = new ArrayList<>();
 
     public void depositar(double valor) {
+        if (valor <= 0) {
+            System.out.println("Valor inválido!");
+            return;
+        }
         saldo += valor;
         movimentos.add(new Movimento(Movimento.Tipo.DEPOSITO, valor));
     }
 
     public void levantar(double valor) {
+        if (valor <= 0) {
+            System.out.println("Valor inválido!");
+            return;
+        }
+        if (valor > saldo) {
+            System.out.println("Saldo insuficiente!");
+            return;
+        }
+    
         saldo -= valor;
         movimentos.add(new Movimento(Movimento.Tipo.LEVANTAMENTO, valor));
     }
